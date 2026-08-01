8fbe9b573f0f04a651d774e50b7075ff1ffe960a
doraimon0 <86925746+doraimon0@users.noreply.github.com>
2026-08-02 00:18:31 +0300

    Delete at.lua

--- at.lua
+++
@@ -1,7 +0,0 @@
-getgenv().mode = "noui"
-getgenv().fps_limit = 30
--- Enter the share code shown on your main account.
-getgenv().share_code = "g264879666"
--- Keep it enabled to continue to get lastest sync data from your main account.
-getgenv().share_autosync = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/doraimon0/xexellent/refs/heads/main/xeft.lua"))()
